94ccc4123a36a55f4afd063b3fab2dcac39a64e9
Royx89 <rogers_89@hotmail.es>
2025-03-25 13:53:16 -0700

    Agrega archivos base del sistema MVC

---
+++ app/core/Model.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../config/database.php';
+
+class Model {
+    protected $db;
+
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->connect();
+    }
+}
